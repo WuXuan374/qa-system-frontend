03afced806786a5626e7117b4e4ebfeca8cb0b0a
WuXuan374 <2953098003@qq.com>
2021-01-24 11:12:25 +0800

    yarn reinstall

--- qa-system/src/App.js
+++ qa-system/src/App.js
@@ -10,15 +10,27 @@ import {
 import {
   options,
 } from './mock/mockData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 // import './mock/mockApi'
 import { apis, prefix } from './api/apis'
 
-
 // 生成路径的过程可以自动化：比如GET方法自动把参数写成?question="" / 配置文件中配置前缀 
 function App() {
   const [question, setQuestion] = useState("")
+  const [keywords, setKeywords] = useState([])
+  
+  useEffect(() =>
+    getKeywordsOptions()
+  , [])
+
+  const getKeywordsOptions = () => {
+    axios.get(`${prefix}${apis.keywords}`)
+      .then((res) => {
+        setKeywords(res)
+      })
+      .catch((reason) => console.error(reason))
+  }
 
   const onSearch = () => {
     console.log('inside')
@@ -46,7 +58,7 @@ function App() {
             value={question}
             style={{ width: 300}}
             placeholder="input here"
-            options={options}
+            options={keywords}
             filterOption={(inputValue, option) => 
               option.value.includes(inputValue)
             }
--- qa-system/src/api/apis.js
+++ qa-system/src/api/apis.js
@@ -1,4 +1,5 @@
 export const apis = {
-  answers: 'api/answers'
+  answers: 'api/answers',
+  keywords: 'api/keywords',
 }
 export const prefix = 'http://127.0.0.1:5000/'
