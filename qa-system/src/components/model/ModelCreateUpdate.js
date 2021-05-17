024b0fbca58d53771e6236a012584c350b3a267e
WuXuan374 <2953098003@qq.com>
2021-05-17 15:33:07 +0800

    feat: Model Create

--- qa-system/src/components/model/ModelCreateUpdate.js
+++ qa-system/src/components/model/ModelCreateUpdate.js
@@ -1,12 +1,32 @@
-import { Button, Form, InputNumber, Input, Card, Breadcrumb, PageHeader } from 'antd';
-import React from 'react';
+import { Button, Form, InputNumber, Input, Card, Breadcrumb, PageHeader, message as antdMessage } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { triggerAPIRequest, apis, HTTP_GET, HTTP_POST } from '../../api/apiConfig';
 import QAPage from '../common/QAPage';
 
 const ModelCreateUpdate = (props) => {
-  console.log(props);
   const { state: formInitialValues } = props.location || {};
+  const [existModelNames, setExistModelNames] = useState();
+
+  const getModelList = () => {
+    triggerAPIRequest(`${apis.model.detail}/`, HTTP_GET)
+      .then((data) => {
+        const names = data && data.map(item => item.name)
+        setExistModelNames(names)
+      })
+  };
+
+  const onSubmit = async () => {
+    await form.validateFields();
+    triggerAPIRequest(`${apis.model.create}/`, HTTP_POST, form.getFieldsValue())
+      .then(({ message }) =>  antdMessage.success(message) )
+      .catch(({ error }) => antdMessage.error(error))
+  }
+  
+  useEffect(() => {
+    getModelList();
+  }, []);
+
   const [form] = Form.useForm();
-  console.log(form.getFieldsValue());
   return (
     <QAPage
       breadCrumb={(
@@ -33,53 +53,93 @@ const ModelCreateUpdate = (props) => {
             layout='horizontal'
             form={form}
             initialValues={formInitialValues}
-            onFinish={() => console.log(form.getFieldsValue())}
+            onFinish={onSubmit}
+            scrollToFirstError
           >
             <Form.Item
               label="模型名称"
               name="name"
               required
+              rules={[{
+                validator(_, value) {
+                  if (!value || !existModelNames || !existModelNames.includes(value)) {
+                    return Promise.resolve();
+                  } else {
+                    return Promise.reject('模型名称必须唯一，不能与已有名称重复');
+                  }
+                }
+              }]}
             >
-              <Input placeholder="请输入唯一模型名称"/>
+              <Input placeholder="请输入唯一模型名称" size="middle" />
             </Form.Item>
             <Form.Item
               label="词向量维度"
               name="word_dimension"
               required
+              rules={[{
+                type: 'number',
+                min: 50,
+                max: 300
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={50} max={300} step={50} />
             </Form.Item>
             <Form.Item
               label="Batch Size"
               name="batch_size"
               required
+              rules={[{
+                type: 'number',
+                min: 20,
+                max: 200
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={20} max={200} step={10}/>
             </Form.Item>
             <Form.Item
               label="Character Embedding Dimension"
               name="character_dimension"
               required
+              rules={[{
+                type: 'number',
+                min: 5,
+                max: 20
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={5} max={20} />
             </Form.Item>
             <Form.Item
               label="Dropout Rate"
               name="dropout_rate"
+              rules={[{
+                type: 'number',
+                min: 0,
+                max: 1
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={0} max={1} step={0.01}/>
             </Form.Item>
             <Form.Item
               label="Learning Rate"
               name="learning_rate"
+              rules={[{
+                type: 'number',
+                min: 0,
+                max: 1
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={0} max={1} step={0.01}/>
             </Form.Item>
             <Form.Item
               label="Context Length"
               name="context_len"
+              rules={[{
+                type: 'number',
+                min: 1,
+                max: 400
+              }]}
             >
-              <InputNumber />
+              <InputNumber min={1} max={400} step={1}/>
             </Form.Item>
             <Form.Item wrapperCol={{ span: 12, offset: 7 }}>
                 <Button type="primary" htmlType="submit">Submit</Button>
